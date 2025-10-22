0793c6c9877d6ad5e0ff82c07253b00eb2caa58b
Sofi <sofiacmariv@gmail.com>
2025-10-22 19:02:07 +0200

    nuevo programa matematico

--- programa.py
+++ programa.py
@@ -1,3 +1,7 @@
-print("añsdjfhañoirh `´pjr +pJ´psfjdañ lsjkdfhña ouhrñ ouasgf ñkjzhsdfñ aljhf ")
-
 # DESARROLLA UN PROGRAMA PYTHON QUE PIDA USUARIO UN NÚMERO Y DIGA SI ESTE ES MÚLTIPLO DE 2
+number = int(input("Introduce un número: "))
+if number % 2 == 0:
+    print(f"El número {number} es múltiplo de 2")
+else:
+    print(f"El número {number} no es múltiplo de 2")
+
