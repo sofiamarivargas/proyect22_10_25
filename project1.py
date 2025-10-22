31501c8357c242e026d0cb0204f478cab9f5b496
Jorge <122154386+jorgealbertceb@users.noreply.github.com>
2025-10-22 18:48:51 +0200

    Add print statement for hacker message

--- project1.py
+++ project1.py
@@ -3,3 +3,6 @@ edad = int(input("Introduce tu edad: "))
 hobbies = str(input("Introduce tus hobbies: "))
 print(f"Tu nombre es {nombre}, tienes {edad} años y tus hobbies son {hobbies}.")
 print("HOLAAA")
+
+
+print("Soy un hacker")
